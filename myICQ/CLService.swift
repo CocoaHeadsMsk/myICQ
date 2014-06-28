cb477431aa65c00979bd2e3efe18e051b1e8eeae
Ruslan Ahapkin <r.akhapkin@corp.mail.ru>
2014-06-28 13:51:45 +0400

    1) Fix contactList.

--- myICQ/CLService.swift
+++ myICQ/CLService.swift
@@ -9,7 +9,11 @@
 import Foundation
 
 class CLService {
-	func contactList() -> Array<Contact> {
-		
+	func contactList() -> Contact[] {
+		var contactList = Contact[]()
+		for _ in 0..10 {
+			contactList += Contact()
+		}
+		return contactList
 	}
 }
