5995c8f0acadaa3d99fc895d19ab3ef511ba5455
Ruslan Ahapkin <r.akhapkin@corp.mail.ru>
2014-06-28 13:42:05 +0400

    1) Add CL service, transport service.

---
+++ myICQ/TransportService.swift
@@ -0,0 +1,13 @@
+//
+//  TransportService.swift
+//  myICQ
+//
+//  Created by Ruslan Ahapkin on 28.06.14.
+//  Copyright (c) 2014 alxsrg.com. All rights reserved.
+//
+
+import Foundation
+
+class TransportService {
+	
+}
